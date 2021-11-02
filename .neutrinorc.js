e4f64bff7c0d6c7fadfeaaa705cbca38176a0d85
Jesper Sørensen <jeppester1@gmail.com>
2021-11-02 14:15:51 +0100

    Remove unused configuration

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -61,16 +61,7 @@ module.exports = {
       neutrino.config.resolve.extensions.add(".ts")
       neutrino.config.module.rule("compile").test(/\.(wasm|mjs|jsx|js|tsx|ts)$/)
       neutrino.config.module.rule("compile").include.clear()
-
       neutrino.config.resolve.alias.set("~", path.resolve(__dirname))
-      neutrino.config.devServer.port(process.env.PORT || 3000)
-      neutrino.config.devServer.host(process.env.PORT ? "0.0.0.0" : "localhost")
-      neutrino.config.devServer.set("sockPort", "location")
-      neutrino.config.devServer.set("disableHostCheck", true)
-      neutrino.config.devServer.set("onListening", () => {
-        // aspnetcore's React SPA boilerplate waits for this non-configurable "magic string" before concluding that the development server is ready
-        console.log("Starting the development server")
-      })
     },
   ],
 }
