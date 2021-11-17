7a2ca3807ab36d16a3efa8104771d9c5854f4b78
Jesper Sørensen <jeppester1@gmail.com>
2021-11-17 09:15:27 +0100

    Alert: Add icon functionality

--- stories/Alert.stories.tsx
+++ stories/Alert.stories.tsx
@@ -1,7 +1,12 @@
 import React from "react"
 import { ComponentStory, ComponentMeta } from "@storybook/react"
+import InformationCircleIcon from "@heroicons/react/solid/InformationCircleIcon"
+import CheckCircleIcon from "@heroicons/react/solid/CheckCircleIcon"
+import ExclamationIcon from "@heroicons/react/solid/ExclamationIcon"
+import XCircleIcon from "@heroicons/react/solid/XCircleIcon"
 
 import Alert from "~/components/Alert"
+import AlertIcon from "~/components/AlertIcon"
 import Variants from "~/const/variants"
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
@@ -17,25 +22,37 @@ const Template: ComponentStory<typeof Alert> = (args) => <Alert {...args} />
 export const Info = Template.bind({})
 Info.args = {
   variant: Variants.Info,
-  children: "Alert text",
+  children: [
+    <AlertIcon icon={InformationCircleIcon} variant={Variants.Info} key="" />,
+    "Alert text",
+  ],
 }
 
 export const Success = Template.bind({})
 Success.args = {
   variant: Variants.Success,
-  children: "Alert text",
+  children: [
+    <AlertIcon icon={CheckCircleIcon} variant={Variants.Success} key="" />,
+    "Alert text",
+  ],
 }
 
 export const Warning = Template.bind({})
 Warning.args = {
   variant: Variants.Warning,
-  children: "Alert text",
+  children: [
+    <AlertIcon icon={ExclamationIcon} variant={Variants.Warning} key="" />,
+    "Alert text",
+  ],
 }
 
 export const Danger = Template.bind({})
 Danger.args = {
   variant: Variants.Danger,
-  children: "Alert text",
+  children: [
+    <AlertIcon icon={XCircleIcon} variant={Variants.Danger} key="" />,
+    "Alert text",
+  ],
 }
 
 export const Primary = Template.bind({})
