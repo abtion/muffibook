198c33d298a79c0c7d89ca2ddbaeeb6328c31099
Jesper Sørensen <jeppester1@gmail.com>
2021-12-15 09:51:15 +0100

    Upgrade storybook and tailwind
    The process here:
    - Upgrade tailwind to 3
    - Tailwind 3 doesn't support postcss 7, so upgrade to postcss 8
    - Storybook uses postcss 7, so we have to resolve postcss to 8 in package.json
    - Storybook successfully builds, but all requests fail
    - Upgrade storybook to 6.4.9 - build succeeds!
    - Tests fail because storybook 6.4.9 comes with react-router 6 which isn't backwards compatible.
    - Update "RenderWithRouteHistory" to comply with new react-router 6 api.
    - Success!
    
    Gotta love software dev.

--- utils/renderWithRouterHistory.tsx
+++ utils/renderWithRouterHistory.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { render as tlRender } from "@testing-library/react"
-import { Router } from "react-router"
+import { Router } from "react-router-dom"
 import { createMemoryHistory, MemoryHistory } from "history"
 
 export interface RenderResult extends ReturnType<typeof tlRender> {
@@ -9,13 +9,17 @@ export interface RenderResult extends ReturnType<typeof tlRender> {
 
 export default function renderWithRouteHistory(
   component: JSX.Element,
-  paths?: string[]
+  paths: string[] = ["/"]
 ): RenderResult {
   const history = createMemoryHistory({
-    initialEntries: [`/`, ...(paths || [""])],
+    initialEntries: paths,
   })
 
-  const context = tlRender(<Router history={history}>{component}</Router>)
+  const context = tlRender(
+    <Router location={paths[0]} navigator={history}>
+      {component}
+    </Router>
+  )
 
   return { history, ...context }
 }
