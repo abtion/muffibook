04043863d846386c0eecbee26dc1a51cff4abd5e
Jesper Sørensen <jeppester1@gmail.com>
2021-12-16 20:46:54 +0100

    ButtonClear: Add box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ module.exports = {
   variants: {
     extend: {
       backgroundColor: ["odd", "active"],
+      boxShadow: ["active"],
     },
   },
   plugins: [require("@tailwindcss/aspect-ratio")],
