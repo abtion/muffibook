62642d713138012625c6335b0b80c735459ab829
Jesper Sørensen <jeppester1@gmail.com>
2022-07-07 09:28:13 +0200

    Update svgr mock

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,5 +1,4 @@
 declare module "*.svg" {
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGAttributes<SVGElement>
-  >
+  type SVGComponent = React.FunctionComponent<React.SVGAttributes<SVGElement>>
+  export default SVGComponent
 }
